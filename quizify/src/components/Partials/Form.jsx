33e29154ce4e801b830be726cf92792f7c086e72
Nasmin <fathimanesmi@gmail.com>
2024-08-23 15:38:06 -0400

    form handlling on submission and add score state to keep track of score of the participant.

--- quizify/src/components/Partials/Form.jsx
+++ quizify/src/components/Partials/Form.jsx
@@ -2,16 +2,18 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import Button from "./Button";
 import Options from "./Options";
+import { questions } from "../../../questions";
+import { useNavigate } from "react-router-dom";
 
-export default function Form() {
+export default function Form(props) {
   // state to select the option
   const [selectedOption, setSelectedOption] = useState("");
+  const navigate = useNavigate();
 
   // Function for handling the option change
   const handleOptionChange = (event) => {
     setSelectedOption("");
     setSelectedOption(event.target.value);
-    console.log(event.target.value);
   };
 
   // Inline style for Form
@@ -22,34 +24,47 @@ export default function Form() {
     alignItems: "center",
   };
 
-  return (
-    <form action="" style={form}>
-      <Options
-        value="option1"
-        label="Option 1"
-        selectedOption={selectedOption}
-        handleOptionChange={handleOptionChange}
-      />
-      <Options
-        value="option2"
-        label="Option 2"
-        selectedOption={selectedOption}
-        handleOptionChange={handleOptionChange}
-      />
-      <Options
-        value="option3"
-        label="Option 3"
-        selectedOption={selectedOption}
-        handleOptionChange={handleOptionChange}
-      />
-      <Options
-        value="option4"
-        label="Option 4"
-        selectedOption={selectedOption}
-        handleOptionChange={handleOptionChange}
-      />
+  
+  // Fucntion to submit the answer of the question
+  const submitAnswer = (event) => {
+    event.preventDefault();
 
-      <Button type="submit" text="Next" />
+    // calculate the updated score
+    const updatedScore = selectedOption === props.answer ? props.score + 1 : props.score;
+
+    // check the user's answer with correct answer
+    if (selectedOption === props.answer){
+      props.setScore(updatedScore)
+    }
+    // render the next question
+    if(props.currentIndex < questions.length - 1){
+      setSelectedOption("")
+      props.setCurrentIndex(props.currentIndex + 1)
+    }else{
+      console.log("quiz completed");
+      console.log("You scored : ", props.score);
+
+      // Navigate to the finish page with updated score
+      navigate('/finish', {state: {score: updatedScore}})
+    }
+  };
+
+  return (
+    <form onSubmit={submitAnswer} style={form}>
+      {props.options.map((opt, index) => (
+        <Options
+          key={index}
+          value={opt}
+          label={opt}
+          selectedOption={selectedOption}
+          handleOptionChange={handleOptionChange}
+        />
+      ))}
+      {props.currentIndex === questions.length - 1 ? (
+        <Button type="submit" text="Finish" />
+      ) : (
+        <Button type="submit" text="Next" />
+      )}
     </form>
   );
 }
