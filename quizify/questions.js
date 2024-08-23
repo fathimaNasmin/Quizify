4ab1a974a9a1c53d6d5f66b8954cb48917c19bb8
Nasmin <fathimanesmi@gmail.com>
2024-08-23 15:36:17 -0400

    add sample question file for testing

---
+++ quizify/questions.js
@@ -0,0 +1,14 @@
+export const questions = [
+  {
+    id: 1,
+    questionText: "What is the capital of France?",
+    options: ["Paris", "London", "Berlin", "Madrid"],
+    answer: "Paris"
+  },
+  {
+    id: 2,
+    questionText: "Which planet is known as the Red Planet?",
+    options: ["Mars", "Earth", "Jupiter", "Venus"],
+    answer:"Mars"
+  },
+];
