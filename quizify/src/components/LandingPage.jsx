6360a922271d6970acaf3468fb0932cea8e1a1d7
Nasmin <fathimanesmi@gmail.com>
2024-08-23 16:38:20 -0400

    handle name input error if name is empty

--- quizify/src/components/LandingPage.jsx
+++ quizify/src/components/LandingPage.jsx
@@ -6,6 +6,8 @@ import { useNavigate } from 'react-router-dom';
 export default function LandingPage(){
     const [name, setName] = useState("");
     const [participants, setParticipants] = useState([]);
+    // state to store error message if the user doesn't enter the name
+    const [error, setError] = useState("");
     const navigate = useNavigate();
 
     // Handle start button click
@@ -13,30 +15,61 @@ export default function LandingPage(){
         if (name.trim() !== ''){
             setParticipants([...participants, {name: name, score: 0}])
             setName("");
+            setError("");
+            console.log(participants)
             navigate('/instruction', {replace:true})
 
+        }else{
+            setError("Name cannot be empty.");
+            console.log(error);
+            
         }
     }
 
-    return <div className="background">
-        <div className='main-container'>
-            <div className="logo-container">
-                <p>Quizify</p>
-            </div>
+    // Handle input change
+    const handleNameInput = (event) =>{
+        setName(event.target.value)
+        if(event.target.value.trim() !== ''){
+            setError("")
+        }
+    }
 
-            <div className='name-container'>
-                <label htmlFor="">Enter your name</label>
-                <input type="text" name="" id="" value={name} 
-                onChange={(event)=>{
-                    setName(event.target.value);
-                }}/>
-            </div>
+    return (
+      <div className="background">
+        <div className="main-container">
+          <div className="logo-container">
+            <p>Quizify</p>
+          </div>
 
-            <div className='start-button-container'>
-                <button type='submit' onClick={handleClick}>Start</button>
-            </div>
+          <div className="name-container">
+            <label htmlFor="">Enter your name</label>
+            <input
+              type="text"
+              name=""
+              id=""
+              value={name}
+              onChange={handleNameInput}
+            />
+            {error && (
+              <p
+                style={{
+                  color: "var(--bg-color-secondary-color)",
+                  fontFamily: "var(--text-font-family)",
+                }}
+              >
+                {error}
+              </p>
+            )}
+          </div>
+
+          <div className="start-button-container">
+            <button type="submit" onClick={handleClick}>
+              Start
+            </button>
+          </div>
         </div>
-    </div>
+      </div>
+    );
 }
 
 //on button click 
